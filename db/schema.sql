84e39635bd306ba666db5e2606194c3746d4bc52
Matt H <matthewdherold@gmail.com>
2021-03-27 12:43:26 -0700

    adding file structure and starting dependencies

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS user_db;
+CREATE DATABASE user_db;
+USE user_db;
+
+CREATE TABLE user_info;
+
+CREATE TABLE eating_info;
+
+CREATE TABLE sleeping_info;
+
+CREATE TABLE spending_info;
