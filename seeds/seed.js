7b49dffe78918250dc874a19809394e3e6daa551
Dorrian Weber <dorrianweber@gmail.com>
2021-03-30 19:51:28 -0700

    Updated sleeping.json seed file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,19 +1,30 @@
 const sequelize = require('../config/connection');
-const seedUser = require('./user');
-const seedSleeping = require('./sleeping');
-const seedEating = require('./eating');
-const seedSpending = require('./spending');
+const { User, Spending, Sleeping, Eating } = require('../models');
+
+const userData = require('./user.json');
+const sleepingData = require('./sleeping.json');
+const eatingData = require('./eating.json');
+const spendingData = require('./spending.json');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await seedUser();
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
 
-  await seedEating();
-
-  await seedSleeping();
-
-  await seedSpending();
+  await Sleeping.bulkCreate(sleepingData, {
+    returning: true,
+  });
+  
+  await Eating.bulkCreate(eatingData, {
+    returning: true,
+  });
+  
+  await Spending.bulkCreate(spendingData, {
+    returning: true,
+  });
 
   process.exit(0);
 };
