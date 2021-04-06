d06c1135ee18309d0e6c220435255c17b1b009de
toasted-coder <christmanholmes21@gmail.com>
2021-04-06 13:53:04 -0700

    added nodemailer to index.js api file, and the server.js file. Fixed mailerRoutes.js so test email is actually being sent. Need to figure out how to implement it into the sign up form, currently just sends me an email everytime server starts.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
-// const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
