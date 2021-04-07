6a02f6a175e825a5d17d7b974efcbbb057f75297
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-04-06 21:49:35 -0700

    Added user name to profile section. Added quotes to eating, sleeping, and spending forms. Added image to the login section

--- controllers/api/eatingRoutes.js
+++ controllers/api/eatingRoutes.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const { Eating } = require('../../models');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Eating } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-router.post('/', withAuth, async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
     const newEat = await Eating.create({
       ...req.body,
@@ -15,7 +15,7 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
-router.get('/', withAuth, async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
     const eatData = await Eating.findAll({
       where: {
@@ -25,7 +25,27 @@ router.get('/', withAuth, async (req, res) => {
     //
     //const allEats = eatData.map((contents) => contents.get({ plain: true }));
     console.log(eatData);
-    return res.json({data: eatData , success: true});
+    return res.json({ data: eatData, success: true });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const eatData = await eat.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!eatData) {
+      res.status(404).json({ message: "No food found with this id!" });
+      return;
+    }
+
+    res.status(200).json(projectData);
   } catch (err) {
     res.status(500).json(err);
   }
