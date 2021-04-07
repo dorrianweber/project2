d60b6c395df4e5bb24c98934382edbbcb04e27db
toasted-coder <christmanholmes21@gmail.com>
2021-04-06 19:07:45 -0700

    WE DID IT GENTS! nodemailer running how it should.

--- controllers/api/mailerRoutes.js
+++ controllers/api/mailerRoutes.js
@@ -2,27 +2,29 @@ const router = require("express").Router();
 const nodemailer = require("nodemailer");
 const mail_pass = process.env.mail_pass;
 
-let transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "healthyhabitsapp123@gmail.com",
-    pass: mail_pass,
-  },
-});
+const mailer = (userData) => {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "healthyhabitsapp123@gmail.com",
+      pass: mail_pass,
+    },
+  });
 
-var mailDetails = {
-  from: "healthyhabitsapp123@gmail.com",
-  to: "christmanholmes21@gmail.com",
-  subject: "Sending Email using Node.js",
-  text: "That was easy!",
-  html: "<b>Hey there! </b><br> This is our first message sent with Nodemailer",
+  var mailDetails = {
+    from: "healthyhabitsapp123@gmail.com",
+    to: userData.email,
+    subject: "Welcome to Healthy Habits!",
+    text: "That was easy!",
+    html: `<b>Hey there! ${userData.name} </b><br> Thanks for signing up for Healthy Habits tracker. We will be using this email address to send you reports on your habits that you are tracking!`,
+  };
+
+  transporter.sendMail(mailDetails, function (error, data) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: ");
+    }
+  });
 };
-
-transporter.sendMail(mailDetails, function (error, data) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Email sent: ");
-  }
-});
-module.exports = router;
+module.exports = mailer;
--- public/js/login.js
+++ public/js/login.js
@@ -1,61 +1,59 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
   if (email && password) {
-    
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
+    const response = await fetch("/api/users/login", {
+      method: "POST",
       body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      
-      document.location.replace('/profile');
+      document.location.replace("/profile");
     } else {
       alert(response.statusText);
     }
   }
 };
-  
+
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const name = document.querySelector("#name-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
 
   if (name && email && password) {
-    if (password.length >= 8){
-      const response = await fetch('/api/users', {
-        method: 'POST',
+    if (password.length >= 8) {
+      const response = await fetch("/api/users", {
+        method: "POST",
         body: JSON.stringify({ name, email, password }),
-        headers: { 'Content-Type': 'application/json' },
+        headers: { "Content-Type": "application/json" },
       });
 
       if (response.ok) {
-        document.location.replace('/profile');
+        const mailer = await fetch("/api/mailerRoutes", {
+          method: "GET",
+        });
+        document.location.replace("/profile");
       } else {
         alert(response.statusText);
-      };
+      }
+    } else {
+      alert("Password must be at least 8 characters");
     }
-    else {
-      alert('Password must be at least 8 characters');
-    };
+  } else {
+    alert("Please fill out name, email, & password fields");
   }
-  else{
-    alert('Please fill out name, email, & password fields');
-  };
 };
 
 document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
 
 document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
